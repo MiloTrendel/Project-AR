f9f94c954b98650169484f098345c6590e8156ea
jolan <105483008+jolanValteau@users.noreply.github.com>
2024-02-26 16:09:32 +0100

    ShowAR default Functions

--- Assets/Scripts/GameState/States/SubStates/GMShowAR.cs
+++ Assets/Scripts/GameState/States/SubStates/GMShowAR.cs
@@ -7,25 +7,6 @@ public class GMShowAR : GMBaseState
 {
     private List<Vector3> Joints;
 
-    public List<Vector3> UpdateJointPositions(bool jointouorientation, SkeletonInfo skeletonInfo)
-    {
-        Debug.Log("======================================================");
-        Joints = new List<Vector3>(21);
-        Vector3[] valSkeleton = skeletonInfo.orientation_joints;
-
-        if (jointouorientation) valSkeleton = skeletonInfo.joints;
-        Vector3 origin = valSkeleton[0];
-
-        foreach (var field in valSkeleton)
-        {
-            Vector3 joint = new Vector3(field.x - origin.x, field.y - origin.y, field.z);
-            Joints.Add(joint);
-            Debug.Log(joint);
-        }
-
-        return Joints;
-    }
-
     public GMShowAR(GameStateContext context, GameStateManager.EGameStates key) : base(context, key)
     {
         GameStateContext Context = context;
@@ -59,5 +40,34 @@ public class GMShowAR : GMBaseState
     {
         Debug.Log("DEBUG functon called");
         nextStateKey = GameStateManager.EGameStates.MainMenu;
+    }
+
+    public List<Vector3> GetLastHandPos(bool jointouorientation, SkeletonInfo skeletonInfo)
+    {
+        Debug.Log("======================================================");
+        Joints = new List<Vector3>(21);
+        Vector3[] valSkeleton = skeletonInfo.orientation_joints;
+
+        if (jointouorientation) valSkeleton = skeletonInfo.joints;
+        Vector3 origin = valSkeleton[0];
+
+        foreach (var field in valSkeleton)
+        {
+            Vector3 joint = new Vector3(field.x - origin.x, field.y - origin.y, field.z);
+            Joints.Add(joint);
+            Debug.Log(joint);
+        }
+
+        return Joints;
+    }
+
+    public void TrySpells(List<Spell> spells)
+    {
+
+    }
+
+    public void DrawSpells(List<Spell> spells)
+    {
+
     }
 }
