6fa2293f6c1c34de1a84def7f4be008d463a92be
jolan <105483008+jolanValteau@users.noreply.github.com>
2024-04-22 17:56:42 +0200

    Stash Particules spawn

--- Assets/Particules/GenericParticule.cs
+++ Assets/Particules/GenericParticule.cs
@@ -12,7 +12,11 @@ public class GenericParticule
         if (ParticuleGO == null)
         {
             GenericPlayer player = GameStateContext.Player1;
-            Vector3 partSpawnPosition = new(player.ParticuleSpawn.position.x + (100 * player.Particules.Count), player.ParticuleSpawn.position.y, player.ParticuleSpawn.position.z);
+            Vector3 partSpawnPosition;
+            if (player.Particules.Count == 0)
+                partSpawnPosition = new(player.ParticuleSpawn.position.x, player.ParticuleSpawn.position.y, player.ParticuleSpawn.position.z);
+            else
+                partSpawnPosition = new(player.Particules[-1].ParticuleGO.transform.position.x + (100), player.ParticuleSpawn.position.y, player.ParticuleSpawn.position.z);
 
             ParticuleGO = GameObject.Instantiate(ParticulePrefab, partSpawnPosition, Quaternion.identity, player.ParticuleSpawn);
         }
