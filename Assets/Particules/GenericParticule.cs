463d2be0bd9d3a0e2c92129769b443f308ce1694
jolan <105483008+jolanValteau@users.noreply.github.com>
2024-05-02 15:59:58 +0200

    Particule Dynamic LifeTime Double Lifetime debug Spell

--- Assets/Particules/GenericParticule.cs
+++ Assets/Particules/GenericParticule.cs
@@ -7,6 +7,8 @@ public class GenericParticule
     public float LifeTime { get; set; } = 10.0f;
     public float Force { get; protected set; } = 1.1f;
 
+    public GenericPlayer player { get; set; }
+
     public GenericParticule(GameObject ParticulePrefab)
     {
         if (ParticuleGO == null)
@@ -17,7 +19,6 @@ public class GenericParticule
                 partSpawnPosition = player.ParticuleSpawn.position + ((Vector3)Random.insideUnitCircle.normalized) * 100;
             else
                 partSpawnPosition = player.ParticuleSpawn.position + ((Vector3)Random.insideUnitCircle.normalized) * 100;
-            //partSpawnPosition = new(player.Particules.Last().ParticuleGO.transform.position.x + (100), player.ParticuleSpawn.position.y, player.ParticuleSpawn.position.z);
 
             ParticuleGO = GameObject.Instantiate(ParticulePrefab, partSpawnPosition, Quaternion.identity, player.ParticuleSpawn);
         }
